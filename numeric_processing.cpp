fbc31493a494fc32b8077ed1e62920c7ee6484bd
MrOcelotGuy <dieartmen@gmail.com>
2025-05-16 11:40:08 -0700

    created the numeric processing hw for some reason didnt push proper;y

--- numeric_processing.cpp
+++ numeric_processing.cpp
@@ -6,4 +6,14 @@ int main()
 {
     ifstream InputFile;
     InputFile.open("Random.txt");
+
+    int example;
+    int example2;
+    
+    InputFile >> example; 
+    cout << example << endl; 
+    InputFile >> example2;
+    cout << example2;
 }
+
+    
