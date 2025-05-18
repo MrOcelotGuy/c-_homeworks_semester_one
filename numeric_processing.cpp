dffe7787ae9d105e9c29dbe577ef6d441fb36cf7
MrOcelotGuy <dieartmen@gmail.com>
2025-05-18 16:36:06 -0700

    added numeric processing for chapter 5

--- numeric_processing.cpp
+++ numeric_processing.cpp
@@ -4,16 +4,31 @@ using namespace std;
 
 int main()
 {
+
+    double total;
+    double average;
+    int count;
+    int hold;
+
     ifstream InputFile;
     InputFile.open("Random.txt");
 
-    int example;
-    int example2;
     
-    InputFile >> example; 
-    cout << example << endl; 
-    InputFile >> example2;
-    cout << example2;
+
+    while(InputFile >> hold)
+    {
+        total += hold;
+        count ++;
+    }
+    
+    InputFile.close();
+
+    average = total/count;
+
+    cout << "The amount of numbers is " << count << endl;
+    cout << "The total quantity is " << total << endl;
+    cout << "The average of all the numbers is " << average << endl;
+
 }
 
     
